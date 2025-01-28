6f1a6d39ba3542e490a1e7da61f62f580c939711
vankos <14087195+vankos@users.noreply.github.com>
2025-01-28 11:58:57 +0700

    try to fix github actions 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,5 +68,4 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
-    debugImplementation(libs.androidx.fragment.ktx)
 }

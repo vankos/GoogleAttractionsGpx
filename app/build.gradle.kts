8ecfed3c0a5ee1158cc6883f6d1fdcf5c9ef2ab3
vankos <14087195+vankos@users.noreply.github.com>
2025-01-30 21:20:05 +0700

    use version from tag

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 28
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = project.findProperty("versionName")?.toString() ?: "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
